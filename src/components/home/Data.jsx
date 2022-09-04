ec24e8e208404ad4fd874dd162d2c57884b5bc63
babinicz <babinicz@onet.pl>
2022-09-04 18:20:07 +0200

    kompresja, zmiany

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import img from "../../assets/6.png";
 import { MdOutlineFrontHand } from "react-icons/md";
+import img1 from "../../assets/18.png";
 
 const Data = () => {
+  const [toggleState, setToggleState] = useState(0);
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
   return (
     <div className="home__data">
       <h1 className="home__title">
@@ -21,9 +27,34 @@ const Data = () => {
         żeby robić stand-up. Od tamtego dnia nigdy nie obejrzałam się za
         siebie."
       </p>
-      <a href="/" className="button button--flex">
+      <a href="#" className="button button--flex" onClick={() => toggleTab(1)}>
         Więcej&nbsp; <i class="uil uil-arrow-up-right"></i>
       </a>
+
+      <div
+        className={
+          toggleState === 1 ? "services__modal active-modal" : "services__modal"
+        }
+      >
+        <div className="services__modal-content">
+          <i
+            onClick={() => toggleTab(0)}
+            className="uil uil-times services__modal-close"
+          ></i>
+          <h3 className="services__modal-title">
+            Jennifer Fulwiler, była ateistyczna blogerka, a obecnie katoliczka
+          </h3>
+          <p className="services__modal-description">
+            Zanim została pisarką, pracowała jako programistka stron
+            internetowych i ukończyła z wyróżnieniem University of Texas na
+            wydziale reklamy ze szczególnym uwzględnieniem nowych mediów.
+            Mieszka z mężem i sześciorgiem małych dzieci w Austin w Teksasie.
+          </p>
+          <ul className="services__modal-services grid">
+            <img src={img1} alt="..." />
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "./services.css";
+import img1 from "../../assets/24.jpg";
+import img2 from "../../assets/25.jpg";
+import img3 from "../../assets/26.jpg";
 
 const Services = () => {
   const [toggleState, setToggleState] = useState(0);
@@ -36,42 +39,16 @@ const Services = () => {
                 onClick={() => toggleTab(0)}
                 className="uil uil-times services__modal-close"
               ></i>
-              <h3 className="services__modal-title">Product Designer</h3>
+              <h3 className="services__modal-title">
+                Historia mojego nawrócenia
+              </h3>
               <p className="services__modal-description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deserunt corrupti molestias totam.
+                Z radością ogłaszam, że napisałam artykuł do bieżącego wydania
+                magazynu Envoy. To najbardziej wyczerpująca wersja mojej
+                historii nawrócenia, jaką dotychczas napisałam.
               </p>
               <ul className="services__modal-services grid">
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
+                <img src={img1} alt="..." />
               </ul>
             </div>
           </div>
@@ -100,42 +77,13 @@ const Services = () => {
                 onClick={() => toggleTab(0)}
                 className="uil uil-times services__modal-close"
               ></i>
-              <h3 className="services__modal-title">Ui/Ux Designer</h3>
+              <h3 className="services__modal-title">Jej historia</h3>
               <p className="services__modal-description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deserunt corrupti molestias totam.
+                Jest stałym gościem w sieciach Relevant Radio i EWTN Radio, a
+                jej historia pojawiła się w telewizji EWTN.
               </p>
               <ul className="services__modal-services grid">
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
+                <img src={img2} alt="..." />
               </ul>
             </div>
           </div>
@@ -144,8 +92,7 @@ const Services = () => {
           <div>
             <i className="uil uil-edit services__icon"></i>
             <h3 className="services__title">
-              Współautorka książek <br /> "Kościół i nowe media" oraz "Od
-              ateisty do katolika"
+              Osobisty blog <br /> ConversionDiary.com
             </h3>
           </div>
           <span onClick={() => toggleTab(3)} className="services__button">
@@ -165,42 +112,15 @@ const Services = () => {
                 onClick={() => toggleTab(0)}
                 className="uil uil-times services__modal-close"
               ></i>
-              <h3 className="services__modal-title">Visual Designer</h3>
+              <h3 className="services__modal-title">Blog</h3>
               <p className="services__modal-description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deserunt corrupti molestias totam.
+                ponad 300 000 odsłon każdego miesiąca, Jennifer zwyciężyła w
+                kategoriach „Najlepszy blog pisany”, „Najbardziej duchowy blog”,
+                „Najlepszy blog kobiety” i „People’s Choice Blog” w katolickich
+                nagrodach nowych mediów.
               </p>
               <ul className="services__modal-services grid">
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </li>
+                <img src={img3} alt="..." />
               </ul>
             </div>
           </div>
